511444437b2cdf80b3e5a36d0778ccc76895d33d
dev3459 <test45@outlook.fr>
2021-02-03 13:26:56 +0100

    Exo 170 Fini

---
+++ bio.php
@@ -0,0 +1,2 @@
+<?php
+echo "Le cookie username comporte le nom : ".$_COOKIE['username'];
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
  * 2. Affichez la valeur de ce cookie avec un simple echo.
  */
 // TODO Votre code ici.
-
+// setcookie("username", "Gaëtan");
+// echo $_COOKIE['username'];
 
 
 /**
@@ -14,3 +15,12 @@
  * 5. Créez une page bio.php et tentez d'afficher le cookie username sur cette page.
  */
 // TODO Votre code ici.
+setcookie('username', 'Gaetan', time() + 180000, "/", 'localhost');
+if(isset($_COOKIE['username'])){
+    echo "Le cookie username a été généré !";
+}else{
+    echo "Il y a eu un soucis lors de la génération du cookies username !";
+}
+?>
+
+<a href="bio.php">Passer à la page bio.php</a>
